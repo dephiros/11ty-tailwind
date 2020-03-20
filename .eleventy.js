05cc43381e5cb425ac0fd517ffcd9da2840a2c53
An Nguyen <an@rover.com>
2020-03-20 08:44:24 -0700

    Use parcel to process all html entries

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,10 @@
-module.exports = {
-    dir: {
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("src/content/**/*.{png,jpg,jpeg,ico}");
+    return {
+      dir: {
         input: "src/content",
-        data: "src/_data",
-        includes: "src/_includes",
-        output: "dist"
-    }
-};
+        output: "dist",
+      },
+    };
+  };
+  
--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,26 @@
-const purgecss = [
-  "@fullhuman/postcss-purgecss",
-  {
-    content: ['./dist/**/*.html'],
-    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-    extensions: ['css', 'html', 'vue'],
-  },
-];
+const postcssPresetEnv = require("postcss-preset-env");
+const purgecss = require("@fullhuman/postcss-purgecss");
+
+const prodPlugins =
+  process.env.NODE_ENV === "production"
+    ? [
+        purgecss({
+          content: ["./_dist/**/*.html"],
+          defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || [],
+          extensions: ["css", "html", "vue"],
+        }),
+      ]
+    : [];
+
 module.exports = {
   plugins: [
     "postcss-import",
+    postcssPresetEnv({
+      features: {
+        "focus-within-pseudo-class": false,
+      },
+    }),
     "tailwindcss",
-    "autoprefixer",
-    // "cssnano",
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+    ...prodPlugins,
   ],
 };
